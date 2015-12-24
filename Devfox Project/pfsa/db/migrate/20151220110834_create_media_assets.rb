532cccf6ab71f717f354fe078d1d0d0e5fc92435
ahtisham hafeez <ahtsham232@gmail.com>
2015-12-24 13:36:29 +0500

    Media assets are defined back

--- Devfox Project/pfsa/db/migrate/20151220110834_create_media_assets.rb
+++ Devfox Project/pfsa/db/migrate/20151220110834_create_media_assets.rb
@@ -1,11 +1,7 @@
 class CreateMediaAssets < ActiveRecord::Migration
-  def up
+  def change
     create_table :media_assets do |t|
 
-      t.string"asset_image"
-      t.string"asset_video"
-      t.string"asset_audio"
-      t.string"asset"
       t.timestamps null: false
     end
   end
