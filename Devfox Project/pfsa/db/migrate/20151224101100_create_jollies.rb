28daad295c5121276aa1921374d08db66840eada
ahtisham hafeez <ahtsham232@gmail.com>
2015-12-24 15:26:38 +0500

    delete model

--- Devfox Project/pfsa/db/migrate/20151224101100_create_jollies.rb
+++ Devfox Project/pfsa/db/migrate/20151224101100_create_jollies.rb
@@ -3,6 +3,7 @@ class CreateJollies < ActiveRecord::Migration
     create_table :jollies do |t|
       t.string "ShexiPc"
 
+      t.string" Abr jolly Pc"
 
       t.string"Jolly lolly"
       t.timestamps null: false
